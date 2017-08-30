9a18ecb18c45d8cb9c30c7c18ce8df8664a01d4b
unknown <Saiidma@gmail.com>
2017-08-30 16:35:01 +0100

    Including Animated

--- src/app.js
+++ src/app.js
@@ -4,25 +4,36 @@ import {
 	StyleSheet,
 	Text,
 	Image,
-	PanResponder
+	PanResponder,
+	Animated
  } from 'react-native';
 
 const fbImage = 'https:\\graph.facebook.com/1405171576/picture?height=500';
 
 class App extends Component {
 	componentWillMount() {
+		this.pan = new Animated.ValueXY();
+
 		this.cardPanResponder = PanResponder.create({
 			onStartShouldSetPanResponder: () => true,
-			onPanResponderMove: (e, gesture) => console.log(gesture.moveX),
+			onPanResponderMove: Animated.event([
+				null,
+				{ dx: this.pan.x, dy: this.pan.y }
+			]),
 			onPanResponderRelease: (e, gesture) => console.log('Release', gesture.moveY)
 		});
 	}
-
 	render() {
+		const AnimatedStyle = {
+			transform: [
+				{ translateX: this.pan.x },
+				{ translateY: this.pan.y },
+			]
+		};
 		return (
-		<View
+		<Animated.View
 			{...this.cardPanResponder.panHandlers}
-			style={styles.card}
+			style={[styles.card, AnimatedStyle]}
 		>
 			<Image
 			style={{ flex: 1 }}
@@ -32,7 +43,7 @@ class App extends Component {
 				<Text style={{ fontSize: 20 }}>Said, 23</Text>
 				<Text style={{ fontSize: 15, color: 'darkgrey' }}>Engineer</Text>
 			</View>
-		</View>
+		</Animated.View>
 	);
 	}
 }
